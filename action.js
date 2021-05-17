3229f1ecc397d6396020356a223abc880d084765
Praveen Francis <67855937+Praveen-Francis@users.noreply.github.com>
2021-05-17 21:48:49 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,9 @@
+function purple(){
+    document.body.style.background="purple"
+}
+function green(){
+    document.body.style.background="green"
+}
+function red(){
+    document.body.style.background="red"
+}
